8ddd13032d435450072018be993074c784b9287e
ishoef <ismailhossennayeb@gmail.com>
2025-07-12 04:11:18 +0300

    Add user registration endpoint and README file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Setup
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hobbyshop.bhkkg8e.mongodb.net/?retryWrites=true&w=majority&appName=HobbyShop`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("UniHostel");
+    const usersCollection = db.collection("users");
+    // User Collection
+
+    // Post Users
+    app.post("/users", async (req, res) => {
+      const email = req.body.email;
+
+      const existUser = await usersCollection.findOne({ email });
+      if (existUser) {
+        return res
+          .status(500)
+          .send({ message: "user already exists", inserted: false });
+      }
+
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,12 +55,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 // Sample Route
 app.get("/", (req, res) => {
